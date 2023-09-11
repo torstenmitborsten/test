80cf6c2bd1ea81a7283259d9dbcd199cfeaf078f
torstenmitborsten <loeschung-scheibe.0f@icloud.com>
2023-09-11 14:04:53 +0200

    jetzt sollten die updates zu yml und den beiden .py dateien am start sein

--- my_source.py
+++ my_source.py
@@ -21,3 +21,17 @@ def euclid(p, q):
     for p_i, q_i in zip(p, q):
         dist += (q_i - p_i) ** 2
     return dist ** 0.5
+
+
+"""Contains code under test for solution_ci."""
+
+def add_func(a, var_two):
+    """
+    Add two variables.
+
+    Args
+        a: first variable
+        var_two: second variable
+
+    """
+    return a + var_two
